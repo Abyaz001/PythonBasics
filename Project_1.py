1dd177feb8121ec673193e69021c38f13cc87d15
Abyaz001 <a.b.y.a.z.0.0.0.0.0@gmail.com>
2024-06-02 14:45:49 +0530

    Add files via upload

---
+++ Project_1.py
@@ -0,0 +1,9 @@
+x = input("Enter the colour--->")
+if(x== "red"):
+    print("stop🛑")
+elif(x=="yellow"):
+    print("slow down🟡")
+elif(x=="green"):
+   print("Go!🟢")
+else:
+    print("Enter valid colour like 🛑🟡🟢")
